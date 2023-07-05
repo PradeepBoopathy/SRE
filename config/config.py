bcc5d4694da3c5dbd386c975700796f39c5e22d6
Pradeep Boopathy <77095302+PradeepBoopathy@users.noreply.github.com>
2023-07-05 20:08:34 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    'host': 'your_rds_endpoint',
+    'port': 3306,
+    'user': 'your_username',
+    'password': 'your_password',
+    'db': 'your_database_name'
+}
+
+ENCRYPTION_KEY = b'ShcbWtSRwgNOy2d6rBXSl9yc928ycYAvSyTsOE5LRIw='
